27b90461623012c2fae1e5dda3bd1935c9055d8b
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-09-27 20:04:08 -0700

    configure angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { MdInputModule, MdDatepickerModule, MdNativeDateModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    NoopAnimationsModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdInputModule
+  ],
+  exports: [
+    MdDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
